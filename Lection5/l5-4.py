9213e722eac17ed42cbd06755ab15a650aa8a642
kolin52 <k.s.anastasov@gmail.com>
2023-08-31 05:59:45 +0300

    third

---
+++ Lection5/l5-4.py
@@ -0,0 +1,9 @@
+#4. Съставете програма, която по въведено трицифренo число проверява дали числото се дели на всяка своя цифра.
+#Във въведеното число да няма цифра 0.
+
+num = input('Write a three-digit number: ')
+
+if int(num)%int(num[0]) == 0 and int(num)%int(num[1]) == 0 and int(num)%int(num[2]) == 0:
+    print('The number you entered is divisible by each of its numbers')
+else:
+    print('The number you entered is not divisible by each of its numbers')
