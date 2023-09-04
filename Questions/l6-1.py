c4888c61864238433b04fc89462cc7c8fc2b9e58
kolin52 <k.s.anastasov@gmail.com>
2023-09-04 05:46:20 +0300

    question

---
+++ Questions/l6-1.py
@@ -0,0 +1,13 @@
+# Може ли да обясниш как работят циклите тук? Така отговорът е 21, а ако var += 1, което е под continue се премести под if var % 2 == 0:, отговорът е 30.
+
+
+var = 10
+for i in range(10):
+    for j in range(2, 10, 1):
+        if var % 2 == 0:
+            continue
+            var += 1
+    var+=1
+else:
+    var+=1
+print(var)
