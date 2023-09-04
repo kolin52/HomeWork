d819b622adfe61b813e0505db20fdfe45b5aef1d
kolin52 <k.s.anastasov@gmail.com>
2023-09-04 06:05:42 +0300

    fourth

---
+++ Lection6/l6-2.py
@@ -0,0 +1,11 @@
+#Задача 2. Да се състави програма, която ще изчисли сумата на 5, въведени от
+#клавиатурата, естествени числа от интервала [10 ..5555].
+
+num1 = int(input('Write digit between 10 and 5555: '))
+num2 = int(input('Write digit between 10 and 5555: '))
+num3 = int(input('Write digit between 10 and 5555: '))
+num4 = int(input('Write digit between 10 and 5555: '))
+num5 = int(input('Write digit between 10 and 5555: '))
+
+sum = num1+num2+num3+num4+num5
+print(sum)
