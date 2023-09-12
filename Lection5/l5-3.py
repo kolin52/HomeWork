2ee9fa3557633e03f0218c151f6388d8b6e0fa73
WWWCourses <ivawebcourses@gmail.com>
2023-09-12 15:17:07 +0300

    Use variables instead of hardcoded values

--- Lection5/l5-3.py
+++ Lection5/l5-3.py
@@ -1,5 +1,5 @@
-#3. Трябва да се напълни цистерна с вода. Имате 2 кофи с вместимост 2 и 3 литра и ги ползвате едновременно. 
-#Да се състави програма, която по въведен обем извежда как ще прелеете течността с тези кофи, т.е. 
+#3. Трябва да се напълни цистерна с вода. Имате 2 кофи с вместимост 2 и 3 литра и ги ползвате едновременно.
+#Да се състави програма, която по въведен обем извежда как ще прелеете течността с тези кофи, т.е.
 #по-колко пъти ще се пълни всяка от кофите.
 #Входни данни: естествено число от интервала [10..9999].
 
@@ -9,15 +9,15 @@ try:
     k2 = 3
     obem_kofi = k1 + k2
     ob_broi = obem//obem_kofi
-    ostatak = obem%5
-    
-    if obem%5 != 0 and ostatak%2 == 0 and ostatak//2 == 1:
+    ostatak = obem%obem_kofi
+
+    if obem%obem_kofi != 0 and ostatak%k1 == 0 and ostatak//k1 == 1:
         print('Each bucket will be full ', ob_broi, 'times and additional one bucket', k1)
-    elif obem%5 != 0 and ostatak%2 == 0 and ostatak//2 == 2:
+    elif obem%obem_kofi != 0 and ostatak%k1 == 0 and ostatak//k1 == 2:
             print('Each bucket will be full ', ob_broi, 'times and additional two buckets 2 l - ', 2*k1, 'liters')
-    elif obem%5 != 0 and ostatak%3 == 0 and ostatak//3 == 1:
+    elif obem%obem_kofi != 0 and ostatak%k2 == 0 and ostatak//k2 == 1:
             print('Each bucket will be full ', ob_broi, 'times and additional one bucket', k2)
-    elif obem%5 == 0:
+    elif obem%obem_kofi == 0:
         print('Each bucket will be full ', ob_broi, 'times' )
     else:
         print('Each bucket will be full ', ob_broi, 'times and remainder is 1 l')
@@ -25,4 +25,3 @@ try:
 
 except:
     print('Enter correct digit')
-    
