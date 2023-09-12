d7001f7a6b2b63106379f9904b34c1387865db74
WWWCourses <ivawebcourses@gmail.com>
2023-09-12 18:52:56 +0300

    Change logic - better hadnle valid user input

--- Lection5/l5-3.py
+++ Lection5/l5-3.py
@@ -3,25 +3,34 @@
 #по-колко пъти ще се пълни всяка от кофите.
 #Входни данни: естествено число от интервала [10..9999].
 
-try:
-    obem = int(input('Write digit between 10 and 9999: '))
-    k1 = 2
-    k2 = 3
-    obem_kofi = k1 + k2
-    ob_broi = obem//obem_kofi
-    ostatak = obem%obem_kofi
 
-    if obem%obem_kofi != 0 and ostatak%k1 == 0 and ostatak//k1 == 1:
-        print('Each bucket will be full ', ob_broi, 'times and additional one bucket', k1)
-    elif obem%obem_kofi != 0 and ostatak%k1 == 0 and ostatak//k1 == 2:
-            print('Each bucket will be full ', ob_broi, 'times and additional two buckets 2 l - ', 2*k1, 'liters')
-    elif obem%obem_kofi != 0 and ostatak%k2 == 0 and ostatak//k2 == 1:
-            print('Each bucket will be full ', ob_broi, 'times and additional one bucket', k2)
-    elif obem%obem_kofi == 0:
-        print('Each bucket will be full ', ob_broi, 'times' )
-    else:
-        print('Each bucket will be full ', ob_broi, 'times and remainder is 1 l')
+### Enter valid value:
+while True:
+    try:
+        obem = int(input('Write digit between 10 and 9999: '))
+        if 10<=obem<=9999:
+            break
+        else:
+            print('Interval must be: [10-9999]')
+    except:
+        print('Enter correct digit')
 
 
-except:
-    print('Enter correct digit')
+### Main program:
+print('Main Program!!!')
+k1 = 2
+k2 = 3
+obem_kofi = k1 + k2
+ob_broi = obem//obem_kofi
+ostatak = obem%obem_kofi
+
+if obem%obem_kofi != 0 and ostatak%k1 == 0 and ostatak//k1 == 1:
+    print('Each bucket will be full ', ob_broi, 'times and additional one bucket', k1)
+elif obem%obem_kofi != 0 and ostatak%k1 == 0 and ostatak//k1 == 2:
+        print('Each bucket will be full ', ob_broi, 'times and additional two buckets 2 l - ', 2*k1, 'liters')
+elif obem%obem_kofi != 0 and ostatak%k2 == 0 and ostatak//k2 == 1:
+        print('Each bucket will be full ', ob_broi, 'times and additional one bucket', k2)
+elif obem%obem_kofi == 0:
+    print('Each bucket will be full ', ob_broi, 'times' )
+else:
+    print('Each bucket will be full ', ob_broi, 'times and remainder is 1 l')
