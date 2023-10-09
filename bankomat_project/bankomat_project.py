027bbeea833090dc340b0ab9b98341d48d26c6df
kolin52 <138013058+kolin52@users.noreply.github.com>
2023-10-09 10:57:53 +0300

    Update bankomat_project.py

--- bankomat_project/bankomat_project.py
+++ bankomat_project/bankomat_project.py
@@ -10,51 +10,50 @@ if name in data.keys():
 else:
             print('Wrong name')
             quit()
-            
-i = 1
-while i < 4:
-            pin_input = int(input('Enter pin:'))
+
+for i in range(3):
+            pin_input = int(input('Enter your pin: '))
             pin1 = int(data[name])
-            
-
-            if pin_input == pin1:
-                        print('Pin code accepted')
+            if pin1 == pin_input:
+                        print('Login Successful')
+                        print("Make a choice - 1 to 3 or exit")
+                        print('1  Balance Inquiry')
+                        print('2  Withdraw')
+                        print('3  Deposit')
+                        
+                        while True:
+                                    try:
+                                                choice = int(input('Enter your choice:'))
+                                              
+                                                if choice == 1:
+                                                            print('Your balance is: ', balance[name])
+                                                            break
+                                                elif choice == 2:
+                                                            withdraw_sum = int(input('Please write withdraw sum: '))
+                                                            balance1 = balance[name] - withdraw_sum
+                                                            print('Your current balance is: ', balance1 )
+                                                            break
+                                                elif choice == 3:
+                                                            deposit_sum = int(input('Please write deposit_sum: '))
+                                                            balance2 = balance[name] + deposit_sum
+                                                            print('Your current balance is: ', balance2 )                                    
+                                                            break
+                                                
+                                                else:
+                                                            print('Wrong choice. Try again. Thank you for banking with us!')
+                                                            break
+                                                            
+                                                            
+                                    except:
+                                                print('Wrong option. Try again')  
+                                                break                        
+                        break
+            elif pin_input != pin1:
+                        print('Wrong pin code.')
                         i += 1
-                        break
+                        
             else:
-                        print('Wrong pin')
-                        print('Your card is blocked')
+                        print('Pin code not accepted')
                         quit()
-                        
-print('Login Successful')
-print("Make a choice - 1 to 3")
-print('1  Balance Inquiry')
-print('2  Withdraw')
-print('3  Deposit')
 
-while True:
-            
-            try:
-                        choice = int(input('Enter your choice:'))
-                      
-                        if choice == 1:
-                                    print('Your balance is: ', balance[name])
-                                    break
-                        elif choice == 2:
-                                    withdraw_sum = int(input('Please write withdraw sum: '))
-                                    balance1 = balance[name] - withdraw_sum
-                                    print('Your current balance is: ', balance1 )
-                                    break
-                        elif choice == 3:
-                                    deposit_sum = int(input('Please write deposit_sum: '))
-                                    balance2 = balance[name] + deposit_sum
-                                    print('Your current balance is: ', balance2 )                                    
-                                    break
-                        else:
-                                    print('Wrong choice. Try again. Thank you for banking with us!')
-                                    break                            
-                                   
-            except:
-                        print('Wrong option. Try again')
                         
-                        break
