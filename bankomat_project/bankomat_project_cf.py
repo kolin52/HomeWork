2cd7d4be6a9342220f37c7e37dbc92107466f60e
Kalin Anastasov <k.s.anastasov@gmail.com>
2023-11-05 20:16:40 +0200

    new

--- bankomat_project/bankomat_project_cf.py
+++ bankomat_project/bankomat_project_cf.py
@@ -10,4 +10,58 @@ class Check_user_pin():
         if name in a:
             print('Your name is accepted')
 
+   
+
+    def check_pin():
+            try:
+                for i in range(3):
+                                        pin = int(input('Write your pin: '))
+                                        pincode = pd.read_csv('/home/ka/users.csv')
+                                        p = pincode['pincode']
+                                        if pin in p:
+                                                print('Login Successful')
+                                                print('Make a choice - 1 to 3 or exit\n 1  Balance Inquiry\n 2  Withdraw\n 3  Deposit')
+                                
+                                                choice = int(input('Enter your choice:'))
+        
+                                                if choice == 1:
+                                                        balance = pd.read_csv('/home/ka/users.csv')
+                                                        b = balance['balance']
+                                                        
+
+                                                        print('Your balance is: ',b[0] )
+                                                        break
+                                                elif choice == 2:
+                                                                withdraw_sum = int(input('Please write withdraw sum: '))
+                                                                balance1 = b[0] - withdraw_sum
+                                                                print('Your current balance is: ', balance1 )
+                                                                break
+                                                elif choice == 3:
+                                                                deposit_sum = int(input('Please write deposit_sum: '))
+                                                                balance2 = b[0] + deposit_sum
+                                                                print('Your current balance is: ', balance2 )                                    
+                                                                break
+                                                
+                                                else:
+                                                                print('Wrong choice. Try again. Thank you for banking with us!')
+                                                                break                 
+                               
+                                                                print('Wrong option. Try again')  
+                                                                break
+                                        elif pin != p[0]:
+                                                                print('Wrong pin code.')
+                                                                i += 1  
+                                        else:
+                                                
+                                            print('Pin code not accepted')
+                                            quit()
+     
+                                        
+                        
+            except:
+                        print('Wrong input')
+                        quit()
+            
+
     check_name()
+    check_pin() 
